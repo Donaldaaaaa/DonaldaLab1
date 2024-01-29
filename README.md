209062d28e03404dee71d542b468051869d6531b
Donaldaaaaa <156346855+Donaldaaaaa@users.noreply.github.com>
2024-01-28 22:20:55 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Donalda Zhuga-COMP229-Lab1
---
+++ public/javascripts/data.js
@@ -0,0 +1,107 @@
+// Donalda | 301367408 | 01-27-2024
+
+homeContent = {
+  welcomeMessage: "Welcome to My Website",
+  welcomeDescription:
+    "Hey there! I'm Donalda, and I love creating cool software. Join me on this journey where tech meets creativity in the world of software development.",
+  missionStatement:
+    "I'm on a mission to use my skills to build software that makes a positive impact—helping businesses, improving lives, and sparking innovation. Let's explore the endless possibilities together!",
+};
+
+aboutMeContent = {
+  selfIntroduction:
+    "I'm a passionate software engineer, dedicated to coding and problem-solving. My journey began in college, writing my first lines of code. Since then, I've embraced continuous learning, exploring various languages and frameworks. Join me in the dynamic world of software development!",
+  resumeUrl: "/images/resume.pdf",
+  education: [
+    {
+      logo: "https://images.shiksha.com/mediadata/images/1503316696php2anIHr.jpeg",
+      name: "Centennial College",
+      Location: "Toronto CA",
+      degree: "Software Engineering Technology",
+      date: "September 2023 - April 2025",
+    },
+  ],
+  workExperiences: [
+    {
+      logo: "https://accurate.id/wp-content/uploads/2023/05/accurate.id-Jenis-Jenis-Software-Secara-Lengkap-yang-Perlu-Anda-Pahami.png",
+      company: "Bankers",
+      location: "Albania",
+      position: "Web Developer",
+      date: "May 2022 - August 2023",
+    },
+  ],
+};
+
+projectsContent = [
+  {
+    name: "E-commerce Website Development",
+    description: "Create a full-fledged e-commerce website for a client who wants to start or upgrade their online store.",
+    screenShotUrl: "/images/p2.png",
+    details: "E-commerce website development involves creating an online store for a client. This includes designing a user-friendly website, adding products, setting up secure payment options, and making sure everything runs smoothly.",
+
+  },
+  {
+    name: "Mobile App for Fitness",
+    description: "Develop a mobile app focused on health and wellness.",
+    screenShotUrl: "/images/p3.png",
+    details: "FitFlow: Your personalized fitness guide. Tailored workouts, live sessions, nutrition tracking, and a vibrant community—all in one app. Achieve your fitness goals with ease. Download now and unleash your best self!",
+ 
+  },
+  {
+    name: "Machine Learning-Powered Recommendation System",
+    description: "Build a recommendation system that utilizes machine learning algorithms to provide personalized recommendations to users.",
+    screenShotUrl: "/images/p1.png",
+    details: "A machine learning-powered recommendation system analyzes user preferences and behaviors to suggest personalized content or products. It helps improve user engagement and satisfaction by offering tailored recommendations based on their past interactions and interests.",
+    
+  },
+];
+
+const servicesList = [
+  {
+    name: "Web Development",
+    photo: "https://wallpaperaccess.com/full/2525117.jpg",
+  },
+  { name: "Graphic Design",
+   photo: "https://tumo.org/wp-content/uploads/2020/07/design_eng.png" },
+  {
+    name: "Mobile App Development",
+    photo: "https://www.wallpapertip.com/wmimgs/160-1606283_web-developer-wallpaper.jpg",
+  },
+  { 
+    name: "Search Engine Optimization (SEO)", 
+    photo: "https://architechsfortheweb.com/images/news/seo-ranking.jpg" },
+
+  {
+    name: "Social Media Marketing",
+    photo: "https://brayve.net/wp-content/uploads/2019/10/9660/social-media-marketing-the-ultimate-guide.jpg-23keepProtocol",
+  },
+
+  {
+    name: "E-commerce Solutions",  
+    photo: "https://www.weverve.com/wp-content/uploads/2020/10/ecommerce-1.jpg",
+
+  },
+
+  { 
+    name: "Content Writing", 
+    photo: "https://www.joshtalks.com/joshkosh/wp-content/uploads/2018/12/Content-writer.jpg" },
+
+  {
+    name: "UI/UX Design",  
+    photo: "https://png.pngtree.com/thumb_back/fw800/background/20210914/pngtree-ui-ux-development-design-background-concept-image_900165.jpg",
+  },
+
+  {
+    name: "Data Analysis",  
+    photo: "https://www.allbusiness.com/media-library/business-analytics.jpg?id=32092749",
+  },
+
+
+];
+
+module.exports = {
+  homeContent,
+  aboutMeContent,
+  projectsContent,
+  servicesList,
+};
